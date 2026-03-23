ebb0f3af42583f8182ee8793f765b4cac1846104
besselink_r <besselink_r@ca01.ub.lan>
2026-03-23 13:37:13 +0800

    Implemented validation logic

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,49 +1,75 @@
-import React, { Component } from 'react';
+import React, { useState , Component } from 'react';
 
 
-class Signup extends Component {
-    constructor (props) {
-        super(props);
-        this.state = {
-            firstName: '',
+const Signup = () => {
+    
+    const [FormData, setFormData] = useState({
+        firstName: '',
             surname: '',
             username: '',
             password: '',
             email: '',
-            errors: {}
-        };
-    }
+    });
+    
+    const [errors, setErrors] = useState({});
+    
+     
+
     handleChange = (e) => {
         const {name, value} = e.target;
-        this.setState({[name]:value})
+        setFormData({...formData,[name]: value});
     }
 
+    const validate = () => {
+        let tempErrors = {};
+        const nameRegex = /^[A-Za-z]{2,}$/;
+        const userRegex = /^[A-Za-z0-9._-]+$/;
+        const passRegex = /^(?=.*[a-z]) (?=.*[A-Z]) (?=.*\d) (?=.*[\W]) . {8,16}$/;
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if(!nameRegex.test(formData.firstName))
+        tempErrors.firstName = "Letters only, min 2 characters";
+        if(!nameRegex.test(formData.surname))
+        tempErrors.surname = "Letters only, min 2 characters";
+        if(!userRegex.test(formData.username))
+            tempErrors.username = "Letters, numbers, (.), (_), (-) only";
+        if(!passRegex.test(formData.password))
+            tempErrors.password = "8-16 characters (Uppercase, Lowercase, Number, Special character";
+        if(!emailRegex.test(formData.email))
+            tempErrors.email ="Must be a valid email format";
+
+        setErrors(tempErrors);
+        return
+        Object.keys(tempErrors).length === 0;
+    }
 
    handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Form Data:", this.state);
-   }
+    if(validate()) {
+        alert("Form submitted successfully");
+        console.log("Final Data:", formData)
+    }
+   };
 
-   render() {
-    const { errors } = this.state;
-
+   
     return (
         <div style={{maxWidth:'400px',margin:'20px auto',fontFamily:'Arial'}}>
             <h2>Create Account</h2>
-            <form onSubmit={this.handleSubmit}>
+            <form onSubmit={handleSubmit}>
                 <div>
                     <label>First Name:</label>
-                    <input name="firstName" onChange={this.handleChange} type="text"/>
+                    <input name="firstName" value={formData.firstName} onChange={handleChange} type="text"/>
                     {errors.firstName && <p style={{color:'red'}}>{errors.firstName}</p>}
                 </div>
                 <div>
                     <label>Surname:</label>
-                    <input name="surname" onChange={this.handleChange} type="text"/>
+                    <input name="surname" value={formData.surname} onChange={this.handleChange} type="text"/>
                     {errors.surname && <p style={{color:'red'}}>{errors.surname}</p>}
                 </div>
                 <div>
                     <label>Username:</label>
-                    <input name="username" onChange={(e)} type = "text" />
+                    <br />
+                    <input name="username" value={formData.username} onChange={(e)} type = "text" />
                     {errors.username && <p style = {{color: 'red'}}>{errors.username}</p>}
                 </div>
                 <div>
@@ -62,6 +88,6 @@ class Signup extends Component {
         </div>
     )
    }
-}
+
 
 export default Signup

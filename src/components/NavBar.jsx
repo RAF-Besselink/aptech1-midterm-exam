948d6476efd479dbd39c44807192b2992cb7ddcd
besselink_r <besselink_r@ca01.ub.lan>
2026-03-23 11:37:07 +0800

    Created navigation to implement routing

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,32 @@
+import {Link} from 'react-router';
+// import './Navbar.css';
+
+const NavBar = () => {
+    return (
+        <nav style={styles.nav}>
+            <Link style={styles.link} to ="/home">Home</Link>
+            
+            <Link style={styles.link} to ="/signup">Signup</Link>
+               
+            <Link style={styles.link} to ="/success">Success</Link>
+               
+            <Link style={styles.link} to ="/profile">Profile</Link>
+               
+        </nav>
+    );
+}
+
+const styles = {
+  nav: {
+    padding: "15px",
+    background: "#333",
+    display: "flex",
+    gap: "20px"
+  },
+  link: {
+    color: "white",
+    textDecoration: "none",
+    fontWeight: "bold"
+  }
+};
+export default NavBar
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+
+
+const Home = () => {
+    return (
+    <>
+    <h1>Welcome! </h1>
+    <h1>This is the Home Page</h1>
+    </>
+    );
+}
+
+
+export default Home
